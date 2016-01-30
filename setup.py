98a30c1a33d33809f1f1897a7323abfd15be59b5
herrersystem <anton.millescamps@gmail.com>
2016-01-30 23:01:57 +0100

    add tf idf in kewords and change num version

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
 setup(name='grsearch',
-	version='0.5',
+	version='0.6',
 	author='herrersystem',
 	author_email='herrersystem@mailoo.org',
 	url="http://github.com/herrersystem/grsearch",
-	keywords='grammatical search find',
+	keywords='grammatical search find tf idf',
 	description='find keyword within string chars or files.',
 	license='MIT',
 	packages=["grsearch"],
